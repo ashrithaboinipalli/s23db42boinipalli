47d7f3cb01f1caab5fff7f59343299781a588048
ashrithaboinipalli <123191179+ashrithaboinipalli@users.noreply.github.com>
2023-04-14 13:16:05 -0500

    updated Books.js

--- controllers/Books.js
+++ controllers/Books.js
@@ -16,9 +16,19 @@ exports.Books_list = async function(req, res) {
     
 //};
 // for a specific Books.
-exports.Books_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: Books detail: ' + req.params.id);
-};
+//exports.Books_detail = function(req, res) {
+//res.send('NOT IMPLEMENTED: Books detail: ' + req.params.id);
+//};
+exports.Books_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Books.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
 // Handle Books create on POST.
 //exports.Books_create_post = function(req, res) {
 //res.send('NOT IMPLEMENTED: Books create POST');
@@ -30,8 +40,8 @@ let document = new Books();
 // Even though bodies can be in many different formats, we will be picky
 // and require that it be a json object
 // {"Books_type":"goat", "cost":12, "size":"large"}
-document.Books_Name = req.body.Books_Nmae;
-document.Books_rate = req.body.Books_cost;
+document.Book_Name = req.body.Book_Name;
+document.Book_rate = req.body.Book_rate;
 document.Author = req.body.Author;
 try{
 let result = await document.save();
@@ -49,8 +59,28 @@ exports.Books_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: Books delete DELETE ' + req.params.id);
 };
 // Handle Books update form on PUT.
-exports.Books_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: Books update PUT' + req.params.id);
+//exports.Books_update_put = function(req, res) {
+//res.send('NOT IMPLEMENTED: Books update PUT' + req.params.id);
+//};
+//Handle Books update form on PUT.
+exports.Books_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await Books.findById( req.params.id)
+// Do updates of properties
+if(req.body.Book_Name)
+toUpdate.Book_Name = req.body.Book_Name;
+if(req.body.Book_rate) toUpdate.Book_rate = req.body.Book_rate;
+if(req.body.Author) toUpdate.Author = req.body.Author;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
 };
 // VIEWS
 // Handle a show all view
