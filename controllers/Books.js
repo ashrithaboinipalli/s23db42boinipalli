14c52078a82340262e7cdb16b3eaee9a175048bc
ashrithaboinipalli <123191179+ashrithaboinipalli@users.noreply.github.com>
2023-04-18 21:18:04 -0500

    added a query code

--- controllers/Books.js
+++ controllers/Books.js
@@ -107,3 +107,17 @@ res.status(500)
 res.send(`{"error": Error deleting ${err}}`);
 }
 };
+
+// Handle a show one view with id specified by query
+exports.Books_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await Books.findById( req.query.id)
+    res.render('Booksdetail',
+    { title: 'Books Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
--- routes/Books.js
+++ routes/Books.js
@@ -3,4 +3,6 @@ const Books_controlers= require('../controllers/Books');
 var router = express.Router();
 /* GET Books */
 router.get('/', Books_controlers.Books_view_all_Page );
+/* GET detail Books page */
+router.get('/detail', Books_controlers.Books_view_one_Page);
 module.exports = router;
