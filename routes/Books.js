0c3a768538edc69ed084e7636414c96b4a11dc94
ashrithaboinipalli <123191179+ashrithaboinipalli@users.noreply.github.com>
2023-04-27 22:11:10 -0500

    secured buttons

--- routes/Books.js
+++ routes/Books.js
@@ -12,14 +12,15 @@ const Books_controlers= require('../controllers/Books');
 var router = express.Router();
 /* GET Books */
 router.get('/', Books_controlers.Books_view_all_Page );
+module.exports = router;
+
 /* GET detail Books page */
 router.get('/detail', Books_controlers.Books_view_one_Page);
 /* GET create Books page */
-router.get('/create', Books_controlers.Books_create_Page);
+router.get('/create',secured, Books_controlers.Books_create_Page);
 /* GET create update page */
 router.get('/update', Books_controlers.Books_update_Page);
 /* GET delete Books page */
-router.get('/delete', Books_controlers.Books_delete_Page);
+router.get('/delete', secured, Books_controlers.Books_delete_Page);
 /* GET update Books page */
 router.get('/update', secured, Books_controlers.Books_update_Page);
-module.exports = router;
